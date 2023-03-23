b6c21e83cb07a46e0466a0ea4007c2af4e95a474
dhanang2112 <dhanang.hadiyanto@gmail.com>
2023-03-23 15:07:36 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,16 @@
 @extends('main.layout')
 @section('content')
+    <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                   <b> {{ Auth::user()->name }} </b>  {{ __('You are logged in!') }}
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <b> {{ Auth::user()->name }} </b> {{ __('You are logged in!') }}
+        </div>
+    </div>
 @endsection
